8cfae9ff464a56e210abc662d4e43f8f20004593
FraiseD <dominic.fraise@thehutgroup.com>
2018-12-09 16:36:20 +0000

    Add checkbox

--- lib/list_item.dart
+++ lib/list_item.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
 
-class ListItem extends StatelessWidget {
+class ListItem extends StatefulWidget {
   final String content;
 
   ListItem(this.content);
 
   @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ListItemState();
+  }
+
+}
+
+class ListItemState extends State<ListItem>{
+  bool isDone = false;
+
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return ListTile(
-        title: Center(
-          child: Text(content),
+      title: Card(
+        child: Row(
+          children: <Widget>[
+            Checkbox(
+              onChanged: (value) {
+                setState(() {
+                  isDone = value;
+                });
+              },
+              value: isDone,
+            ),
+            Text(widget.content),
+          ],
         ),
-      );
-
+      ),
+    );
   }
 }
