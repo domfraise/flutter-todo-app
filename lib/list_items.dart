ac5388b04f14b81bbd0eee974709d97c002d292f
FraiseD <dominic.fraise@thehutgroup.com>
2018-12-15 12:31:08 +0000

    Use listBuilder for ListView

--- lib/list_items.dart
+++ lib/list_items.dart
@@ -5,18 +5,18 @@ import './list_item.dart';
 class ListItems extends StatelessWidget {
   final List<String> listItems;
   final Function removeItem;
+
   ListItems(this.listItems, this.removeItem);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Flexible(
-      child: ListView(
-        children: listItems
-            .map(
-              (element) => ListItem(element, removeItem),
-            )
-            .toList(),
+      child: ListView.builder(
+        itemCount: listItems.length,
+        itemBuilder: (context, i) {
+          return ListItem(listItems[i], removeItem);
+        },
       ),
     );
   }
